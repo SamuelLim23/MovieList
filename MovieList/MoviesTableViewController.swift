826a831c891dd76a755b865f3c72e771b5db3e71
Samuel Lim <samuel.lim23@d155.org>
2022-10-21 08:09:49 -0500

    added connection

---
+++ MovieList/MoviesTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MoviesTableViewController.swift
+//  MovieList
+//
+//  Created by SAMUEL LIM on 10/21/22.
+//
+
+import UIKit
+
+struct Movie {
+    // ...
+}
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieTitle: UILabel!
+    
+}
+
+class MoviesTableViewController: UITableViewController {
+    
+}
